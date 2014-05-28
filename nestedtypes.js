7ecb8e1847465f3f3d734d271471c0566afb00f8
Vlad Balin <gaperton@gmail.com>
2014-05-28 18:20:06 +0300

    Moved back to NestedTypes namespace in case of script tag.

--- nestedtypes.js
+++ nestedtypes.js
@@ -9,7 +9,8 @@
         factory( exports, require( 'backbone' ), require( 'underscore' ) );
     }
     else{
-        factory( root.Backbone, root.Backbone, root._ );
+        root.NestedTypes = {};
+        factory( root.NestedTypes, root.Backbone, root._ );
     }
 }( this, function( exports, Backbone, _ ){
     'use strict';

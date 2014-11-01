d1d070bdf2dbf4e885dc6d528947bd981a9b08ca
Vlad Balin <gaperton@gmail.com>
2014-11-01 16:33:37 +0200

    Fix for #48
    custom validate() is not called inside isValid() #48

--- nestedtypes.js
+++ nestedtypes.js
@@ -417,7 +417,7 @@
             },
 
             isValid : function( options ){
-                return ModelProto.isValid( options ) && _.every( this.attribute, function( attr ){
+                return ModelProto.isValid.call( this, options ) && _.every( this.attribute, function( attr ){
                     if( attr && attr.isValid ){
                         return attr.isValid( options );
                     }

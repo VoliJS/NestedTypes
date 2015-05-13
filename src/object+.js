3351d42b051e2fdcb645a1273d3318cf7b6677d0
Vlad Balin <gaperton@gmail.com>
2015-05-13 20:01:18 +0300

    - added support for forward declarations - added function override warnings - added 'assign' polyfill - replaced Object.xcopy with assign - Object.extend.attach( Ctor1, Ctor2, ... ) to make any object extendable

--- src/object+.js
+++ src/object+.js
@@ -1,4 +1,36 @@
-Object.xMap = function( dest, source, fun, context ){
+if (!Object.assign) {
+  Object.defineProperty(Object, 'assign', {
+    enumerable: false,
+    configurable: true,
+    writable: true,
+    value: function(target, firstSource) {
+      'use strict';
+      if (target === undefined || target === null) {
+        throw new TypeError('Cannot convert first argument to object');
+      }
+
+      var to = Object(target);
+      for (var i = 1; i < arguments.length; i++) {
+        var nextSource = arguments[i];
+        if (nextSource === undefined || nextSource === null) {
+          continue;
+        }
+
+        var keysArray = Object.keys(Object(nextSource));
+        for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
+          var nextKey = keysArray[nextIndex];
+          var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+          if (desc !== undefined && desc.enumerable) {
+            to[nextKey] = nextSource[nextKey];
+          }
+        }
+      }
+      return to;
+    }
+  });
+}
+
+Object.xmap = function( dest, source, fun, context ){
     context || ( context = null );
 
     for( var name in source ){
@@ -11,19 +43,15 @@ Object.xMap = function( dest, source, fun, context ){
     return dest;
 };
 
-Object.xCopy = function( dest ){
-    for( var i = 1; i < arguments.length; i++ ){
-        var source = arguments[ i ];
-
-        for( var name in source ){
-            source.hasOwnProperty( name ) && ( dest[ name ] = source[ name ] );
-        }
+Object.xgetPropertyDescriptor = function( obj, prop ){
+    for( var desc; !desc && obj; obj = Object.getPrototypeOf( obj ) ){
+        desc = Object.getOwnPropertyDescriptor( obj, prop );
     }
 
-    return dest;
-};
+    return desc;
+}
 
-Object.xEvery = function( obj, fun, context ){
+Object.xevery = function( obj, fun, context ){
     if( obj.every ) return obj.every( fun, context );
 
     for( var name in source ){
@@ -35,16 +63,16 @@ Object.xEvery = function( obj, fun, context ){
     return true;
 };
 
-Object.xisliteral = function( value ){
-    return value && typeof value === 'object' &&  Object.getPrototypeOf( value ) === Object.prototype;
+Object.xisLiteral = function( value ){
+    return value && typeof value === 'object' && Object.getPrototypeOf( value ) === Object.prototype;
 };
 
-Object.xMerge = function( destination, source ){
+Object.xmerge = function( destination, source ){
     Object.xmap( destination, source, function( value, name ){
         if( !destination.hasOwnProperty( name ) ){
             return value;
         }
-        else if( Object.isLiteral( destination[ name ] ) && Object.isLiteral( value ) ){
+        else if( Object.xisLiteral( destination[ name ] ) && Object.isLiteral( value ) ){
             return Object.xmerge( destination[ name ], value );
         }
     });
@@ -52,20 +80,25 @@ Object.xMerge = function( destination, source ){
     return destination;
 };
 
+Object.xwarnings = {
+    overrideMethodWithValue : function( Ctor, name, value ){
+        console.warn( '[Type Warning] Base class method overriden with value in Object.extend({ ' + name + ' : ' + value + ' }); Object =', Ctor.prototype );
+    }
+};
+
 Object.extend = ( function(){
     function Class(){
         this.initialize.apply( this, arguments );
     }
 
-    Class.prototype.initialize = function(){};
-
     // Backbone-style extend with native properties and late definition support
-    Class.extend = function( protoProps, staticProps ) {
+    function extend( protoProps, staticProps ) {
         var Parent = this === Object ? Class : this,
             Child;
 
         if( typeof protoProps === 'function' ){
             Child = protoProps;
+            protoProps = null;
         }
         else if( protoProps && protoProps.hasOwnProperty( 'constructor' ) ){
             Child = protoProps.constructor;
@@ -74,7 +107,7 @@ Object.extend = ( function(){
             Child = function Constructor(){ return Parent.apply( this, arguments ); };
         }
 
-        Object.xcopy( Child, Parent );
+        Object.assign( Child, Parent );
 
         Child.prototype = Object.create( Parent.prototype );
         Child.prototype.constructor = Child;
@@ -83,20 +116,53 @@ Object.extend = ( function(){
         protoProps && Child.define( protoProps, staticProps );
 
         return Child;
-    };
+    }
 
-    Class.define = Class.extend.define = function( protoProps, staticProps ) {
-        Object.xcopy( this.prototype, protoProps );
-        Object.xcopy( this, staticProps );
+    function warnOnError( value, name ){
+        var prop = Object.xgetPropertyDescriptor( this.prototype, name );
+
+        if( prop ){
+            var baseIsFunction = typeof prop.value === 'function',
+                valueIsFunction = typeof value === 'function';
+
+            if( baseIsFunction && !valueIsFunction ){
+                Object.xwarnings.overrideMethodWithValue( this, name, prop );
+            }
+        }
+
+        return value;
+    }
+
+    function define( protoProps, staticProps ) {
+        Object.xmap( this.prototype, protoProps, warnOnError, this );
+        Object.xmap( this, staticProps, warnOnError, this );
 
         Object.defineProperties( this.prototype,
-            Object.xmap( {}, protoProps.properties, function( spec ){
+            Object.xmap( {}, protoProps.properties, function( spec, name ){
+                var prop = Object.xgetPropertyDescriptor( this.prototype, name );
+
+                if( prop && typeof prop.value === 'function' ){
+                    Object.xwarnings.overrideMethodWithValue( this, name, prop );
+                }
+
                 return spec instanceof Function ? { get : spec } : spec;
-            })
+            }, this )
         );
 
         return this;
+    }
+
+    extend.attach = function(){
+        for( var i = 0; i < arguments.length; i++ ){
+            var Ctor = arguments[ i ];
+
+            Ctor.extend || ( Ctor.extend = extend );
+            Ctor.define || ( Ctor.define = define );
+            Ctor.prototype.initialize || ( Ctor.prototype.initialize = function(){} );
+        }
     };
 
-    return Class.extend;
-}() );
+    extend.attach( Class );
+
+    return extend;
+})();

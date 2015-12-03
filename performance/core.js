0375187181ddf61971dee38255a7ff22820a1757
Vlad Balin <gaperton@gmail.com>
2015-12-03 01:19:24 -0500

    updated performance core
    - implemented adaptive iterations calculation algorithm.

--- performance/core.js
+++ performance/core.js
@@ -1,6 +1,13 @@
-function measure( fun, context ){
+function emptyTest( n, context ){
+    for( var i = 0; i < n; i++ ){
+        // do nothing
+    }
+}
+
+
+function measure( fun, context, iterations ){
     const start = window.performance.now();
-    fun( context );
+    fun( iterations, context );
     return window.performance.now() - start;
 }
 
@@ -10,22 +17,53 @@ export const Test = Model.extend( {
     defaults : {
         executedAt : Date.value( null ),
         name      : String,
-        time      : Integer.value( null ),
+        time      : Number.value( null ),
+        count     : Integer,
+        iterations : Integer,
         faster    : Number.value( null ),
         exception : Error.value( null ),
         init      : Function.has.toJSON( false ),
-        test      : Function.has.toJSON( false )
+        test      : Function.has.toJSON( false ).value( emptyTest )
     },
 
-    run(){
+    properties : {
+        ops : function(){
+            return this.time ? Integer( this.count * 1000 / this.time ) : 0;
+        }
+    },
+
+
+    addIterations( iterations ){
+            const context = this.init( iterations ) || {},
+                  time = measure( this.test, iterations, context );
+
+            this.time += time;
+            this.count += iterations;
+    },
+
+    run( iterations ){
         this.transaction( () =>{
             this.exception = null;
-            this.time = 0;
+            this.count = this.time = 0;
             this.executedAt = new Date();
 
             try{
-                const context = this.init() || {};
-                this.time = Math.max( measure( this.test, context ), 1 );
+                var fixedIterations = iterations || this.iterations;
+                if( fixedIterations ){
+                    addIterations( fixedIterations );
+                }
+                else{
+                    // estimate amount of iterations required for 3 seconds...
+                    for( let n = 100; this.time < 200; n *= 2 ){
+                        addIterations( n );
+                    }
+
+                    if( this.time < 3000 ){
+                        const ops = this.ops;
+                        this.count = this.time = 0;
+                        addIterations( ops * 3 );
+                    }
+                }
             }
             catch( e ){
                 this.exception = e;
@@ -35,10 +73,10 @@ export const Test = Model.extend( {
 
     collection : {
         whoIsFaster( thanMe ){
-            const time = thanMe && thanMe.time;
+            const ops = thanMe && thanMe.ops;
 
             this.transaction( () =>{
-                this.tests.each( test => test.faster = time && test.time ? time / test.time : null );
+                this.tests.each( test => test.faster = ops && test.ops ? test.ops / ops : null );
             } );
         }
     }
@@ -48,10 +86,11 @@ export const Group = Model.extend( {
     idAttribute : 'name',
 
     defaults : {
-        lastExecuted : Date.value( null ),
+        executedAt : Date.value( null ),
         name     : String,
         tests    : Test.Collection,
-        selected : Test.from( 'tests' )
+        selected : Test.from( 'tests' ),
+        iterations : Integer
     },
 
     initialize(){
@@ -62,7 +101,7 @@ export const Group = Model.extend( {
 
     run(){
         this.executedAt = new Date();
-        this.tests.each( test => test.run() );
+        this.tests.each( test => test.run( this.iterations ) );
     }
 } );
 
@@ -73,10 +112,10 @@ function oneAttrModel(){
     return Model.defaults({ a : 0 });
 }
 
-function createModel( Model ){
+function createModel( count, Model ){
     var m;
 
-    for( var i = 0; i < 50000; i++ ){
+    for( var i = 0; i < count; i++ ){
         m = new Model();
     }
 }
---
+++ test/collections.js
@@ -0,0 +1,7 @@
+var Nested = require( '../nestedtypes' ),
+    expect = require( 'chai' ).expect,
+    sinon = require( 'sinon' );
+
+describe( 'Collections', function(){
+   describe( 'Create' )
+});

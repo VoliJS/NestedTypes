e3802dc00b0c57790def81a874679c1d80ee3288
Vlad Balin <gaperton@gmail.com>
2015-12-10 18:59:05 -0500

    updated performance tests

--- performance/main.js
+++ performance/main.js
@@ -2,114 +2,351 @@ define( function( require, exports, module ){
     var Nested   = require( '../nestedtypes' ),
         Backbone = require( 'backbone' );
 
+    var NLarge = Nested.Model.extend({
+        defaults : {
+            a1 : 1, a2 : 2, a3 : 3, a4: 4, a5 : 5, a6: 6, a7: 7, a8: 8, a9: 9, a10 : 10,
+            b1 : 1, b2 : 2, b3 : 3, b4: 4, b5 : 5, b6: 6, b7: 7, b8: 8, b9: 9, b10 : 10
+        },
+
+        updateSet : function(){
+            this.set({
+                a1 : this.a1 + 1,
+                a2 : this.a2 + 1,
+                a3 : this.a3 + 1,
+                a4 : this.a4 + 1,
+                a5 : this.a5 + 1
+            });
+        },
+
+        updateTransaction : Nested.transaction(function(){
+            this.a1 = this.a1 + 1;
+            this.a2 = this.a2 + 1;
+            this.a3 = this.a3 + 1;
+            this.a4 = this.a4 + 1;
+            this.a5 = this.a5 + 1;
+        }),
+
+        updateAdHocTransaction : function(){
+            this.transaction( function(){
+                this.a1 = this.a1 + 1;
+                this.a2 = this.a2 + 1;
+                this.a3 = this.a3 + 1;
+                this.a4 = this.a4 + 1;
+                this.a5 = this.a5 + 1;
+            }, {} );
+        },
+
+        save(){}
+    });
+
+    var BLarge = Backbone.Model.extend({
+        defaults : {
+            a1 : 1, a2 : 2, a3 : 3, a4: 4, a5 : 5, a6: 6, a7: 7, a8: 8, a9: 9, a10 : 10,
+            b1 : 1, b2 : 2, b3 : 3, b4: 4, b5 : 5, b6: 6, b7: 7, b8: 8, b9: 9, b10 : 10
+        },
+
+        updateSet : function(){
+            this.set({
+                a1 : this.get( 'a1' ) + 1,
+                a2 : this.get( 'a2' ) + 1,
+                a3 : this.get( 'a3' ) + 1,
+                a4 : this.get( 'a4' ) + 1,
+                a5 : this.get( 'a5' ) + 1
+            });
+        },
+
+        save(){}
+    });
+
+    var BLargeCollection = Backbone.Collection.extend({
+        model : BLarge
+    });
+
+
+    var NSmall = Nested.Model.extend({
+        defaults : {
+            a1 : 1
+        },
+
+        save(){}
+    });
+
+    var BSmall = Backbone.Model.extend({
+        defaults : {
+            a1 : 1
+        },
+
+        save(){}
+    });
+
+    var BSmallCollection = Backbone.Collection.extend({
+        model : BSmall
+    });
+
     describe( 'Flat models', function(){
         this.timeout( 100000 );
 
-        describe( 'primitive types', function(){
-            var NLarge, BLarge, NSmall, BSmall;
-
-            function makeDefinitions(){
-                NLarge = Nested.Model.extend({
-                    defaults : {
-                        a1 : 1, a2 : 2, a3 : 3, a4: 4, a5 : 5, a6: 6, a7: 7, a8: 8, a9: 9, a10 : 10,
-                        b1 : 1, b2 : 2, b3 : 3, b4: 4, b5 : 5, b6: 6, b7: 7, b8: 8, b9: 9, b10 : 10
-                    },
-
-                    updateSet : function(){
-                        this.set({
-                            a1 : this.a1 + 1,
-                            a2 : this.a2 + 1,
-                            a3 : this.a3 + 1,
-                            a4 : this.a4 + 1,
-                            a5 : this.a5 + 1
-                        });
-                    },
-
-                    updateTransaction : Nested.transaction(function(){
-                        this.a1 = this.a1 + 1;
-                        this.a2 = this.a2 + 1;
-                        this.a3 = this.a3 + 1;
-                        this.a4 = this.a4 + 1;
-                        this.a5 = this.a5 + 1;
-                    }),
-
-                    updateAdHocTransaction : function(){
-                        this.transaction( function(){
-                            this.a1 = this.a1 + 1;
-                            this.a2 = this.a2 + 1;
-                            this.a3 = this.a3 + 1;
-                            this.a4 = this.a4 + 1;
-                            this.a5 = this.a5 + 1;
-                        }, {} );
-                    }
-                });
-
-                BLarge = Backbone.Model.extend({
-                    defaults : {
-                        a1 : 1, a2 : 2, a3 : 3, a4: 4, a5 : 5, a6: 6, a7: 7, a8: 8, a9: 9, a10 : 10,
-                        b1 : 1, b2 : 2, b3 : 3, b4: 4, b5 : 5, b6: 6, b7: 7, b8: 8, b9: 9, b10 : 10
-                    },
-
-                    updateSet : function(){
-                        this.set({
-                            a1 : this.get( 'a1' ) + 1,
-                            a2 : this.get( 'a2' ) + 1,
-                            a3 : this.get( 'a3' ) + 1,
-                            a4 : this.get( 'a4' ) + 1,
-                            a5 : this.get( 'a5' ) + 1
-                        });
-                    }
-                });
-
-                NSmall = Nested.Model.extend({
-                    defaults : {
-                        a1 : 1
-                    }
-                });
-
-                BSmall = Backbone.Model.extend({
-                    defaults : {
-                        a1 : 1
-                    }
-                });
-            }
-
+        describe( 'Create performance', function(){
             var n, b;
 
-            describe( '1-attr model, 1M create', function(){
-                makeDefinitions();
-
+            describe( '1-attr model, 50K new', function(){
                 it( 'Backbone', function(){
-                    for( var i = 0; i < 1000000; i++ ){
+                    for( var i = 0; i < 50000; i++ ){
                         b = new BSmall();
                     }
-                });
+                } );
 
                 it( 'Nested', function(){
-                    for( var i = 0; i < 1000000; i++ ){
+                    for( var i = 0; i < 50000; i++ ){
                         n = new NSmall();
                     }
-                });
-            });
+                } );
+            } );
 
-            describe( '20-attrs model, 1M create', function(){
-                makeDefinitions();
+            describe( '20-attrs model, 50K new', function(){
 
                 it( 'Backbone', function(){
-                    for( var i = 0; i < 1000000; i++ ){
+                    for( var i = 0; i < 50000; i++ ){
                         b = new BLarge();
                     }
-                });
+                } );
 
                 it( 'Nested', function(){
-                    for( var i = 0; i < 1000000; i++ ){
+                    for( var i = 0; i < 50000; i++ ){
                         n = new NLarge();
                     }
-                });
-            });
+                } );
+            } );
+
+            describe( '1-attr model, 50K collection.create', function(){
+                it( 'Backbone', function(){
+                    var c = new BSmallCollection();
+                    for( var i = 0; i < 50000; i++ ){
+                        c.create();
+                    }
+                } );
+
+                it( 'Nested', function(){
+                    var c = new NSmall.Collection();
+
+                    for( var i = 0; i < 50000; i++ ){
+                        c.create();
+                    }
+                } );
+            } );
+
+            describe( '20-attr model, 50K collection.create', function(){
+                it( 'Backbone', function(){
+                    var c = new BLargeCollection();
+                    for( var i = 0; i < 50000; i++ ){
+                        c.create();
+                    }
+                } );
+
+                it( 'Nested', function(){
+                    var c = new NLarge.Collection();
+
+                    for( var i = 0; i < 50000; i++ ){
+                        c.create();
+                    }
+                } );
+            } );
+
+            describe( '1-attr model, 50K collection reset', function(){
+                var arr = [];
+                for( var i = 0; i < 50000; i++ ){
+                    arr.push({ a1 : i });
+                }
+
+                it( 'Backbone', function(){
+                    var c = new BSmallCollection();
+                    c.reset( arr );
+                } );
+
+                it( 'Nested', function(){
+                    var c = new NSmall.Collection();
+                    c.reset( arr );
+                } );
+            } );
+
+            describe( '20-attr model, 50K collection reset', function(){
+                var arr = [];
+                for( var i = 0; i < 50000; i++ ){
+                    arr.push({ a1 : i, a2 : i, a3: i, a4 : i, a5 : i, a6 : i, a7 : i, a8: i, a9 : i, a10 : i});
+                }
+
+                it( 'Backbone', function(){
+                    var c = new BLargeCollection();
+                    c.reset( arr );
+                } );
+
+                it( 'Nested', function(){
+                    var c = new NLarge.Collection();
+                    c.reset( arr );
+                } );
+            } );
+
+            describe( '1-attr model, 50K collection set/update', function(){
+                var arr = [], update = [];
+                for( var i = 0; i < 50000; i++ ){
+                    arr.push({ id: i, a1 : i });
+                    update.push({ id: i, a1 : i + 1 });
+                }
+
+                it( 'Backbone', function(){
+                    var c = new BSmallCollection();
+                    c.set( arr );
+                    c.set( update );
+                } );
+
+                it( 'Nested', function(){
+                    var c = new NSmall.Collection();
+                    c.set( arr );
+                    c.set( update );
+                } );
+
+            } );
+
+            describe( '20-attr model, 50K collection set/update', function(){
+                var arr = [], update = [];
+                for( var i = 0; i < 100000; i++ ){
+                    arr.push({ id : i, a1 : i, a2 : i, a3: i, a4 : i, a5 : i, a6 : i, a7 : i, a8: i, a9 : i, a10 : i});
+                    i++;
+                    update.push({ id : i - 1, a1 : i, a2 : i, a3: i, a4 : i, a5 : i, a6 : i, a7 : i, a8: i, a9 : i, a10 : i});
+                }
+
+                it( 'Backbone', function(){
+                    var c = new BLargeCollection();
+                    c.set( arr );
+                    c.set( update );
+                } );
+
+                it( 'Nested', function(){
+                    var c = new NLarge.Collection();
+                    c.set( arr );
+                    c.set( update );
+                } );
+            } );
+        });
+
+        describe( 'Update performance', function(){
+            var n, b;
+
+            describe( '1-attr model, 100K collection.create', function(){
+                it( 'Backbone', function(){
+                    var c = new BSmallCollection();
+                    for( var i = 0; i < 100000; i++ ){
+                        c.create();
+                    }
+                } );
+
+                it( 'Nested', function(){
+                    var c = new NSmall.Collection();
+
+                    for( var i = 0; i < 100000; i++ ){
+                        c.create();
+                    }
+                } );
+            } );
+
+            describe( '20-attr model, 100K collection.create', function(){
+                it( 'Backbone', function(){
+                    var c = new BLargeCollection();
+                    for( var i = 0; i < 100000; i++ ){
+                        c.create();
+                    }
+                } );
+
+                it( 'Nested', function(){
+                    var c = new NLarge.Collection();
+
+                    for( var i = 0; i < 100000; i++ ){
+                        c.create();
+                    }
+                } );
+            } );
+
+            describe( '1-attr model, 100K collection reset', function(){
+                var arr = [];
+                for( var i = 0; i < 100000; i++ ){
+                    arr.push({ a1 : i });
+                }
+
+                it( 'Backbone', function(){
+                    var c = new BSmallCollection();
+                    c.reset( arr );
+                } );
+
+                it( 'Nested', function(){
+                    var c = new NSmall.Collection();
+                    c.reset( arr );
+                } );
+            } );
+
+            describe( '20-attr model, 100K collection reset', function(){
+                var arr = [];
+                for( var i = 0; i < 100000; i++ ){
+                    arr.push({ a1 : i, a2 : i, a3: i, a4 : i, a5 : i, a6 : i, a7 : i, a8: i, a9 : i, a10 : i});
+                }
+
+                it( 'Backbone', function(){
+                    var c = new BLargeCollection();
+                    c.reset( arr );
+                } );
+
+                it( 'Nested', function(){
+                    var c = new NLarge.Collection();
+                    c.reset( arr );
+                } );
+            } );
+
+            describe( '1-attr model, 100K collection set/update', function(){
+                var arr = [], update = [];
+                for( var i = 0; i < 100000; i++ ){
+                    arr.push({ id: i, a1 : i });
+                    update.push({ id: i, a1 : i + 1 });
+                }
+
+                it( 'Backbone', function(){
+                    var c = new BSmallCollection();
+                    c.set( arr );
+                    c.set( update );
+                } );
+
+                it( 'Nested', function(){
+                    var c = new NSmall.Collection();
+                    c.set( arr );
+                    c.set( update );
+                } );
+
+            } );
+
+            describe( '20-attr model, 100K collection set/update', function(){
+                var arr = [], update = [];
+                for( var i = 0; i < 200000; i++ ){
+                    arr.push({ id : i, a1 : i, a2 : i, a3: i, a4 : i, a5 : i, a6 : i, a7 : i, a8: i, a9 : i, a10 : i});
+                    i++;
+                    update.push({ id : i - 1, a1 : i, a2 : i, a3: i, a4 : i, a5 : i, a6 : i, a7 : i, a8: i, a9 : i, a10 : i});
+                }
+
+                it( 'Backbone', function(){
+                    var c = new BLargeCollection();
+                    c.set( arr );
+                    c.set( update );
+                } );
+
+                it( 'Nested', function(){
+                    var c = new NLarge.Collection();
+                    c.set( arr );
+                    c.set( update );
+                } );
+            } );
+        });
+
+        describe( 'Update performance', function(){
+            var n, b;
 
             describe( '1-attr model, 1M .set( "a1", number )', function(){
-                makeDefinitions();
 
                 var b = new BSmall();
 
@@ -137,7 +374,8 @@ define( function( require, exports, module ){
             });
 
             describe( '20-attrs model, 1M .set( "a1", number )', function(){
-                makeDefinitions();
+
+
 
                 var b = new BLarge();
 
@@ -165,7 +403,6 @@ define( function( require, exports, module ){
             });
 
             describe( '20-attrs model, 10M .get( "a1" )', function(){
-                makeDefinitions();
 
                 var b = new BLarge({ a1: 1 }), x;
 
@@ -193,7 +430,6 @@ define( function( require, exports, module ){
             });
 
             describe( 'both models, 1M read and write', function(){
-                makeDefinitions();
 
                 it( 'Backbone', function(){
                     var l = new BLarge(), s = new BSmall();
@@ -224,7 +460,6 @@ define( function( require, exports, module ){
             });
 
             describe( '1M 5-attr transactional updates', function(){
-                makeDefinitions();
 
                 it( 'Backbone', function(){
                     var l = new BLarge();
@@ -259,5 +494,7 @@ define( function( require, exports, module ){
                 });
             });
         });
+
+
     });
 });

dbe9383501fc2e9838903aeadbe5f1490a5928ef
Vlad Balin <gaperton@gmail.com>
2016-07-28 14:34:59 -0400

    Added ids to the models

--- performance/complex.js
+++ performance/complex.js
@@ -1,7 +1,6 @@
 define( function( require, exports, module ){
     var Nested   = require( '../nestedtypes' ),
         Model = Nested.Model;
-
     /********************
      * Model definitions
      */
@@ -26,8 +25,8 @@ define( function( require, exports, module ){
             largeData = [];
 
         for( var i = 0; i < 50000; i++ ){
-            smallData.push({ a : i });
-            largeData.push({
+            smallData.push({ id : i, a : i });
+            largeData.push({ id : i,
                 a0 : i, a1 : i, a2 : i, a3 : i, a4: i, a5 : i, a6: i, a7: i, a8: i, a9: i,
                 b0 : i, b1 : i, b2 : i, b3 : i, b4: i, b5 : i, b6: i, b7: i, b8: i, b9: i
             });
@@ -36,25 +35,32 @@ define( function( require, exports, module ){
         smallData = JSON.stringify( smallData );
         largeData = JSON.stringify( largeData );
 
+        var small, large;
+
+        beforeEach( function(){
+            small = JSON.parse( smallData );
+            large = JSON.parse( largeData );
+        });
+
         describe( 'Create 50K collection', function(){
             it( '1 attribute model', function(){
-                var smallCollection = new SmallFlatModel.Collection( JSON.parse( smallData ) );
+                var smallCollection = new SmallFlatModel.Collection( small );
             });
 
             it( '20 attribute model', function(){
-                var largeCollection = new LargeFlatModel.Collection( JSON.parse( largeData ) );
+                var largeCollection = new LargeFlatModel.Collection( large );
             });
         });
 
         describe( 'Fetch 50K collection', function(){
             it( '1 attribute model', function(){
                 var smallCollection = new SmallFlatModel.Collection();
-                smallCollection.set( JSON.parse( smallData ) );
+                smallCollection.set( small );
             });
 
             it( '20 attribute model', function(){
                 var largeCollection = new LargeFlatModel.Collection();
-                largeCollection.set( JSON.parse( largeData ) );
+                largeCollection.set( large );
             });
         });
 
@@ -63,21 +69,21 @@ define( function( require, exports, module ){
 
         describe( 'Update 50K collection', function(){
             it( '1 attribute model', function(){
-                _smallCollection.set( JSON.parse( smallData ) );
+                _smallCollection.set( small );
             });
 
             it( '20 attribute model', function(){
-                _largeCollection.set( JSON.parse( largeData ) );
+                _largeCollection.set( large );
             });
         });
 
         describe( 'Reset 50K collection', function(){
             it( '1 attribute model', function(){
-                _smallCollection.reset( JSON.parse( smallData ) );
+                _smallCollection.reset( small );
             });
 
             it( '20 attribute model', function(){
-                _largeCollection.reset( JSON.parse( largeData ) );
+                _largeCollection.reset( large );
             });
         });
     });
@@ -143,52 +149,51 @@ define( function( require, exports, module ){
 
         var treeData, listData;
 
-        treeData = JSON.stringify( createTree( 6 ) );
-        listData = JSON.stringify( createList( 1000 ) );
+        treeJSON = JSON.stringify( createTree( 6 ) );
+        listJSON = JSON.stringify( createList( 1000 ) );
 
-        describe( 'Create structure', function(){
-            it( 'Linked list', function(){
-                var list;
+        var _tree, _list;
 
-                for( var i = 0; i < 1000; i++ )
-                    list = new LinkedList( JSON.parse( listData ) );
-            });
+        describe( '1000 elements linked list, 1000 times + parse time', function(){
+            var list;
 
-            it( 'Wide tree', function(){
-                var tree;
-                tree = new Tree( JSON.parse( treeData ) );
-            });
-        });
-
-        describe( 'Fetch structure', function(){
-            it( 'Linked list', function(){
+            it( 'Create from JSON', function(){
                 for( var i = 0; i < 1000; i++ ){
-                    var list = new LinkedList();
-                    list.set( JSON.parse( listData ) );
+                    list = new LinkedList( JSON.parse( listJSON ) );
                 }
             });
 
-            it( 'Wide tree', function(){
-                var tree = new Tree();
-                tree.set( JSON.parse( treeData ) );
+            it( 'Fetch empty from JSON', function(){
+                for( var i = 0; i < 1000; i++ ){
+                    list = new LinkedList();
+                    list.set( JSON.parse( listJSON ) );
+                }
+            });
+
+            it( 'Update with JSON', function(){
+                for( var i = 0; i < 1000; i++ ){
+                    list.set( JSON.parse( listJSON ) );
+                }            
             });
         });
 
-        describe( 'Update structure', function(){
-            var _list, _tree;
-
-            before( function(){
-                _list = new LinkedList( JSON.parse( listData ) );
-                _tree = new Tree( JSON.parse( treeData ) );
+        describe( 'Wide model tree with 10 childs, 100K elements', function(){
+            var data, tree;
+            beforeEach( function(){
+                data = JSON.parse( treeJSON );
             });
 
-            it( 'Linked list', function(){
-                for( var i = 0; i < 1000; i++ )
-                    _list.set( JSON.parse( listData ) );
+            it( 'Create from JSON', function(){
+                tree = new Tree( data );
             });
 
-            it( 'Wide tree', function(){
-                _tree.set( JSON.parse( treeData ) );
+            it( 'Fetch empty from JSON', function(){
+                tree = new Tree();
+                tree.set( data );
+            });
+
+            it( 'Update with JSON', function(){
+                tree.set( data );
             });
         });
     });
@@ -206,23 +211,24 @@ define( function( require, exports, module ){
             }
         });
 
-        function createTree( level ){
+        function createTree( level, id ){
             var l = level - 1;
             return {
+                id : id || 0,
                 time : new Date(),
                 author_id : level,
                 text : 'hjkfshdkhjfksdhkj fhsdjkhfsdjk hjfksdhjk hfjsdkhjk hfjdskhjkhj fdssdffsdn,m nm, nm, nv,xvcvcx',
                 replies : l ? [
-                    createTree( l ),
-                    createTree( l ),
-                    createTree( l ),
-                    createTree( l ),
-                    createTree( l ),
-                    createTree( l ),
-                    createTree( l ),
-                    createTree( l ),
-                    createTree( l ),
-                    createTree( l )
+                    createTree( l, 0 ),
+                    createTree( l, 1 ),
+                    createTree( l, 2 ),
+                    createTree( l, 3 ),
+                    createTree( l, 4 ),
+                    createTree( l, 5 ),
+                    createTree( l, 6 ),
+                    createTree( l, 7 ),
+                    createTree( l, 8 ),
+                    createTree( l, 9 )
                 ] : []
             }
         }
@@ -244,65 +250,180 @@ define( function( require, exports, module ){
 
         var _comment, _short, _mid, _long;
 
-        describe( 'Create and update', function(){
-            it( 'Create 100K Tree', function(){
-                _comment  = new Comment( JSON.parse( treeData ) );
+        describe( '100K Model/Collection Tree', function(){
+            var data, collection;
+            beforeEach( function(){
+                data = JSON.parse( treeData );
             });
 
-            it( 'Update 100K Tree', function(){
-                _comment.set( JSON.parse( treeData ) );
-                _comment = null;
+            it( 'Create from JSON', function(){
+                collection = new Comment.Collection( data );
+                collection = null;
             });
 
-            it( 'Create 100 elements of 1000 items', function(){
-                _short = new Comment.Collection( JSON.parse( shortList ) );
+            it( 'Fetch empty from JSON', function(){
+                collection = new Comment.Collection();
+                collection.set( data );
             });
 
-            it( 'Update 100 elements of 1000 items', function(){
-                _short.set( JSON.parse( shortList ) );
-                _short = null;
-            });
-
-            it( 'Create 1000 elements of 100 items', function(){
-                _mid = new Comment.Collection( JSON.parse( midList ) );
-            });
-
-            it( 'Update 1000 elements of 100 items', function(){
-                _mid.set( JSON.parse( midList ) );
-                _mid = null;
-            });
-
-            it( 'Create 10000 elements of 10 items', function(){
-                _long = new Comment.Collection( JSON.parse( longList ) );
-            });
-
-
-            it( 'Update 10000 elements of 10 items', function(){
-                _long.set( JSON.parse( longList ) );
-                _long = null;
+            it( 'Update with JSON', function(){
+                collection.set( data );
+                collection = null;
             });
         });
 
-        describe( 'Fetch structure', function(){
-            it( '100K Tree', function(){
-                var comment  = new Comment();
-                comment.set( JSON.parse( treeData ) );
+        describe( '100 elements Collection of 1000 items Model/Collection tree', function(){
+            var data, collection;
+            beforeEach( function(){
+                data = JSON.parse( shortList );
             });
 
-            it( '100 elements of 1000 items', function(){
-                var comments = new Comment.Collection(  );
-                comments.set( JSON.parse( shortList ) );
+            it( 'Create from JSON', function(){
+                collection = new Comment.Collection( data );
+                collection = null;
             });
 
-            it( '1000 elements of 100 items', function(){
-                var comments = new Comment.Collection(  );
-                comments.set( JSON.parse( midList ) );
+            it( 'Fetch empty from JSON', function(){
+                collection = new Comment.Collection();
+                collection.set( data );
             });
 
-            it( '10000 elements of 10 items', function(){
-                var comments = new Comment.Collection(  );
-                comments.set( JSON.parse( longList ) );
+            it( 'Update with JSON', function(){
+                collection.set( data );
+                collection = null;
             });
+        });
+
+        describe( '1000 elements Collection of 100 items Model/Collection tree', function(){
+            var data, collection;
+            beforeEach( function(){
+                data = JSON.parse( midList );
+            });
+
+            it( 'Create from JSON', function(){
+                collection = new Comment.Collection( data );
+                collection = null;
+            });
+
+            it( 'Fetch empty from JSON', function(){
+                collection = new Comment.Collection();
+                collection.set( data );
+            });
+
+            it( 'Update with JSON', function(){
+                collection.set( data );
+                collection = null;
+            });
+        });
+
+        describe( '10 000 elements Collection of 10 items Model/Collection tree', function(){
+            var data, collection;
+            beforeEach( function(){
+                data = JSON.parse( longList );
+            });
+
+            it( 'Create from JSON', function(){
+                collection = new Comment.Collection( data );
+                collection = null;
+            });
+
+            it( 'Fetch empty from JSON', function(){
+                collection = new Comment.Collection();
+                collection.set( data );
+            });
+
+            it( 'Update with JSON', function(){
+                collection.set( data );
+                collection = null;
+            });
+        });
+    });
+
+    describe( 'Users directory', function(){
+        var User = Model.extend({
+            idAttribute: 'user_id',
+            defaults: {
+                created_at : Date,
+                updated_at : Date,
+                username   : String,
+                password   : String,
+                fname      : String,
+                lname      : String,
+                email      : String,
+                active     : Number,
+                'default'    : Number,
+                guid        : '',
+                old_7password : String,
+                created_by : null,
+                domain_id: 0,
+                user_type  : Number,
+                user_types : Array,
+                permissions: {},
+                selectedEncoders : Nested.Collection.subsetOf( '~encoders' ),
+                roles : Nested.Collection.subsetOf( '~roles' ),
+                settings: Model.defaults({
+                    TZ: '',
+                    DefaultMetadataState: 'off',
+                    LoudnessMeterState: 'off',
+                    OrderChannels: 'name',
+                    PlayLiveLastChannel: false,
+                    SendExportNotification: true,
+                    DisplayPlayerSpeed: false
+                })
+            }
+        });
+
+        var Dummy = Nested.Model.extend({
+            attributes : {
+                name : ''
+            }
+        });
+
+        var Store = Nested.Store.defaults({
+            encoders : Dummy.Collection,
+            roles : Dummy.Collection
+        });
+
+        var store;
+        if( Nested.store ){
+            store = Nested.store = new Store();
+        }
+        else{
+            store = Nested.Store.global = new Store();
+        }
+
+        var dummies = [];
+        for( var i = 0; i < 100; i++ ){
+            dummies.push( { id : i, name : 'name' + i });
+        }
+
+        store.encoders = dummies;
+        store.roles = dummies;
+
+        var usersJSON;
+
+        beforeEach( function(){
+            usersJSON = [], user = new User();
+
+            for( var i = 0; i < 10000; i++ ){
+                user.id = i;
+                usersJSON.push( user.toJSON() );
+            }
+        });
+
+        var collection;
+
+        it( 'Creates users collection from JSON', function(){
+            collection = new User.Collection( usersJSON );
+        } );
+
+        it( 'Fetches users collection from JSON', function(){
+            collection = new User.Collection();
+            collection.set( usersJSON );
+        } );
+
+        it( 'Updates users collection', function(){
+            collection.set( usersJSON );
         });
     });
 });
